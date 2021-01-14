bcf5cd4295ce42229582e6abbb4fdc0190d8f7c1
Swapneil Meshram <74193695+SwapneilMeshram@users.noreply.github.com>
2021-01-15 02:11:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1, pig1;
+// ;var backgroundImg;
+var ground1;
+
+function preload() {}
+
+function setup(){
+    var canvas = createCanvas(200,600);
+
+    //;platform = new Ground(150, 305, 300, 170);
+}
+
+function draw(){
+    background("black");
+    // ;Call  local-Scope functions
+    // ;functions disabled for test purposes
+
+    /**
+     *   box1.display();
+     *  box2.display();
+     *  ground.display();
+     *   pig1.display();
+     *   log1.display();
+     *   box3.display();
+     *   box4.display();
+     *   pig3.display();
+     *   log3.display();
+     *   box5.display();
+     *   log4.display();
+     *   log5.display();
+     *   ground1.display();
+     **/
+}
